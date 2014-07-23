3c25855de945653c21113e57d6b47476b66620ce
anichalakova <anichalakova@users.noreply.github.com>
2014-07-23 21:34:09 +0300

    added background

--- js/game.js
+++ js/game.js
@@ -4,3 +4,11 @@ var ctx = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 480;
 document.body.appendChild(canvas);
+
+//Insert background images
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+ bgReady = true;
+};
+bgImage.src = "images/bg.jpg";
