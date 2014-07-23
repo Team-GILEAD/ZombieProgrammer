e55cf08930181cb3ad7f00630393be5566807291
anichalakova <anichalakova@users.noreply.github.com>
2014-07-24 00:12:10 +0300

    added hero and monster objects

--- js/game.js
+++ js/game.js
@@ -13,11 +13,41 @@ bgImage.onload = function () {
 };
 bgImage.src = "images/bg.jpg";
 
+// Hero image
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "images/hero.png";
+
+// Monster image
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function () {
+	monsterReady = true;
+};
+monsterImage.src = "images/monster.png";
+
+// Game objects
+var hero = {
+	speed: 256 // movement in pixels per second
+};
+var monster = {};
+
 // Draw everything
 var render = function () {
- if (bgReady) {
-  ctx.drawImage(bgImage, 0, 0);
- }
+	 if (bgReady) {
+	  ctx.drawImage(bgImage, 0, 0);
+	 }
+ 
+ 	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
 };
 
 // The main game loop
