3d5aad13bfd4637debba3ab5d7286b9881192d56
Anton Cholakov <anton_cholakov@ymail.com>
2014-07-23 16:07:10 +0300

    Created index.html and game.js files.. added some first bg.jpg.. Added Canvas creation in the js file

---
+++ js/game.js
@@ -0,0 +1,6 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 1024;
+canvas.height = 480;
+document.body.appendChild(canvas);
