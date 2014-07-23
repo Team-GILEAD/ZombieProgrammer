97ea351398f14a79ec0edd35645cb8c3808b17c5
PetarPopstoyanov <p.popstoyanov@abv.bg>
2014-07-24 00:26:08 +0300

    Add browsers supporting

--- js/game.js
+++ js/game.js
@@ -60,7 +60,14 @@ var main = function () {
 render();
 
  then = now;
+ 
+ // Request to do this again ASAP
+	requestAnimationFrame(main);
 };
+
+// Cross-browser support for requestAnimationFrame
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
 // Play the game
 var then = Date.now();
