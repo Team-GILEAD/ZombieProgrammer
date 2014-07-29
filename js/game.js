84d1ec932864c8dd23a012d5b46c5aae93337b4b
Anton Cholakov <anton_cholakov@ymail.com>
2014-07-29 20:07:07 +0300

    Fixed #13

--- js/game.js
+++ js/game.js
@@ -184,15 +184,19 @@ var update = function (modifier) {
 			
 		if (38 in keysDown) { // Player holding up
 			hero.y -= hero.speed * modifier;	
+			heroImage.src = "images/heroRight.png";
 		}
 		if (40 in keysDown) { // Player holding down
 			hero.y += hero.speed * modifier;
+			heroImage.src = "images/heroRight.png";
 		}
 		if (37 in keysDown) { // Player holding left
 			hero.x -= hero.speed * modifier;
+			heroImage.src = "images/heroLeft.png";
 		}
 		if (39 in keysDown) { // Player holding right
 			hero.x += hero.speed * modifier;
+			heroImage.src = "images/heroRight.png";
 		}
 		if (80 in keysDown) {
 			pause = true;
